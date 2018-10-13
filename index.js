050a7b74ced21491e9f2e9e83b4e8f05be3ba2e3
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-10-13 17:17:52 +0300

    Created router
    Created router and added morgan dendency for logging

--- index.js
+++ index.js
@@ -3,31 +3,41 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
 
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+
+
+morgan.token('content', function (req) {
+  return JSON.stringify(req.body)
+})
+
+// Enable logging for requests
+app.use(morgan(':method :url :content :status :res[content-length] - :response-time ms'))
+
+
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+// use environment variables to pass user credentials
+const mongoUrl = process.env.MONGO_DB_URI
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Database connection established')
+  })
+  .catch( err => {
+    console.error(err)
+  })
 
 
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
--- models/blog.js
+++ models/blog.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose')
 
-
-if ( process.env.NODE_ENV !== 'production' ) {
-  require('dotenv').config()
-}
-// use environment variables to pass user credentials
-const mongoUrl = process.env.MONGO_DB_URI
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-
 const Blog = mongoose.model('Blog', {
   title: String,
   author: String,
