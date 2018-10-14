9d52ff6716dd873114f6aff198682bab41122806
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-10-14 16:21:55 +0300

    Added api unit tests
    Added api unit tests for get request to /api/blogs

---
+++ tests/blogs_api.test.js
@@ -0,0 +1,89 @@
+const supertest = require('supertest')
+const { app, server } = require('../index')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12
+  },
+  {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10
+  },
+  {
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0
+  },
+  {
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2
+  }
+]
+
+
+beforeAll(async () => {
+  await Blog.remove({})
+  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are six blogs', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body.length).toBe(initialBlogs.length)
+})
+
+test('the first blog is about HTTP methods', async () => {
+  const expected= {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10
+  }
+  const response = await api.get('/api/blogs')
+  const contents = response.body.map(blog => {
+    return {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes
+    }
+  })
+  expect(contents).toContainEqual(expected)
+})
+
+
+afterAll(() => {
+  server.close()
+})
