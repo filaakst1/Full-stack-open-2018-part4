5943b29035c41fd27cc3d93466a5cedc48449697
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-10-22 21:43:07 +0300

    Fixed unit test
    Fixed unit test by removing automatically inserted text.

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -161,7 +161,7 @@ describe('when there is initially some blogs saved', async () => {
     })
   })
 
-  describe.only('when there is initially one user at db', async () => {
+  describe('when there is initially one user at db', async () => {
     beforeAll(async () => {
       await User.remove({})
       const user = new User({ username: 'root', password: 'sekret', adult: true })
